e82527adc81ca6edb6d58df11802fc4ac88d2ef0
Ranvijay Singh <ras009@ucsd.edu>
2017-02-02 19:14:59 -0800

    node.js lab

--- routes:project.js
+++ routes:project.js
@@ -39,8 +39,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
-app.get('/project/:name', project.viewProject);
- 
+app.get('/project', project.viewProject);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
